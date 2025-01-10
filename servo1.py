4bc5d20164fadf6d5056e7482b66a80aa4325fa1
CharChips <109170618+CharChips@users.noreply.github.com>
2025-01-10 11:34:23 +0530

    Update servo1.py

--- servo1.py
+++ servo1.py
@@ -2,8 +2,8 @@ import RPi.GPIO as GPIO
 import time
 
 # Setup the GPIO pin for the servo
-servo_pin = 13
-GPIO.setmode(GPIO.BCM)
+servo_pin = 3
+GPIO.setmode(GPIO.BOARD)
 GPIO.setup(servo_pin, GPIO.OUT)
 
 # Setup PWM process
@@ -15,14 +15,14 @@ pwm.start(7)
 try:
     # Clockwise motion
     print("Clockwise Motion:")
-    for ii in range(2, 13):  # Increment duty cycle from 2.0 to 12.0
+    for ii in range(1, 13):  # Increment duty cycle from 2.0 to 12.0
         pwm.ChangeDutyCycle(ii)  # Rotate to specified angle
         print(f"Duty Cycle: {ii}")
         time.sleep(1)
 
     # Anticlockwise motion
     print("\nAnticlockwise Motion:")
-    for ii in range(12, 1, -1):  # Decrement duty cycle from 12.0 to 2.0
+    for ii in range(12, 0, -1):  # Decrement duty cycle from 12.0 to 2.0
         pwm.ChangeDutyCycle(ii)  # Rotate to specified angle
         print(f"Duty Cycle: {ii}")
         time.sleep(1)
